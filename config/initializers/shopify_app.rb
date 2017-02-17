e548d17252731fa50bdcc0994be2fb324e588272
alexgaudiosi <alexg@wemakewebsites.com>
2017-02-17 14:40:31 +0000

    production.rb added

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  # config.application_name = "My Shopify App"
+  config.application_name = "My Shopify App"
   config.api_key = SHOPIFY_KEY
   config.secret = SHOPIFY_SECRET
   config.scope = 'read_customers, read_orders, write_products'
